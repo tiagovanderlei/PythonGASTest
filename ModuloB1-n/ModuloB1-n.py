70fee2e4d9673bc0258b9df6760eb571ee7530fd
tiagovanderlei <tiagovanderleidearruda@gmail.com>
2015-08-12 14:16:22 -0300

    fim estudo modulo B

--- ModuloB1-n/ModuloB1-n.py
+++ ModuloB1-n/ModuloB1-n.py
@@ -96,6 +96,79 @@ print Sprites.__module__
 from math import sin
 print sin.__module__
 
+# Heranca -- classe pai entre parenteses apos o nome da classe
 
+class Pai:
+    Nome='Carlos'
+
+class Filha(Pai):
+    Nome = 'Luciana'
+    Olhos = 'castanhos'
+
+class Neta(Filha):
+    Nome = 'Maria'
+
+
+print Neta.Nome, Filha.Nome, Pai.Nome
+print Neta.Olhos, Filha.Olhos # Pai nao possui olhos, e nao herdou
+
+print issubclass(Neta, Filha) # retorna se A eh subclasse de B
+
+print 'olha, ' \
+    'esta quebra funciona !'   # \ -- continua na linha abaixo
+
+
+
+class Op_Basicas:
+    def __init__(self, valor):
+        self.valor = valor
+    def __add__(self, other):
+        return self.valor + other.valor
+    def __mul__(self, other):
+        return self.valor * other.valor
+
+a = Op_Basicas(56)
+b = Op_Basicas(-2)
+
+print a + b
+
+class Op_Basicas2:
+    def __init__(self, valor):
+        self.valor = valor
+    def __mul__(self, other):
+        return self.valor * other.valor
+
+c = Op_Basicas2(10)
+
+
+print a+c
+#print c+a
+
+# Portanto, ao aplicar operacao, eh utilizada a funcao __sum__ ou __mul__ do operador a esquerda.
+
+# callable :: objetos capazes de carregar nro indeterminado de args
+print callable(Op_Basicas)
+print callable('teste')
+
+
+print callable(a) # instancias, normalmente nao sao callable
+
+### nao vou ver como tornar instancias callable. Todavia, basta sobrecarregar o metodo __call__ :: pulei (pag. 32 --)
+
+# __str__ -- sobrecarrega print. Exemplo
+
+class Teste:
+    def __str__(self):
+        print 'sou uma orgulhosa instancia de %s' %self.__class__
+
+test = Teste()
+print test
+
+
+# pg37 ...
+
+ # -- pulei: fim
+
+ # Modulo C --- por trabalhar com Desktop, aparentemente, nao irei estudar no momento.
 
 # ate pag. 40
